be1096b09e691e2305b806062bcf220ec27f6bf8
Thanh Nha (Will) Phan <phanthanhnha1712000@gmail.com>
2026-02-14 16:59:20 -0500

    Handle invalid request body in GlobalExceptionHandler. Introduce ErrorDto

--- src/main/java/com/codewithmosh/store/controllers/GlobalExceptionHandler.java
+++ src/main/java/com/codewithmosh/store/controllers/GlobalExceptionHandler.java
@@ -4,12 +4,20 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.codewithmosh.store.dtos.ErrorDto;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    /**
+     * This catches exception from @Valid annotation for argument validation
+     * @param exception
+     * @return
+     */
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String,String>> handleValidationErrors(
         MethodArgumentNotValidException exception
@@ -19,5 +27,12 @@ public class GlobalExceptionHandler {
             errors.put(error.getField(), error.getDefaultMessage());
         });
         return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorDto> handleUnreadableMessage() {
+        return ResponseEntity.badRequest().body(
+            new ErrorDto("Invalid request body")
+        );
     }
 }
